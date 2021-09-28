55f2d68b198544c1be1a2a5d8e59290512ced91b
Koray Kolburan <80157575+koraykolburan@users.noreply.github.com>
2021-09-28 12:36:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function multiplyBy() {
+  num1 = document.getElementById("firstNumber").value;
+  num2 = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML = num1 * num2;
+}
